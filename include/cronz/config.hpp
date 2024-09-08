1813123b4de6fef05f6cc983d72c49b7a4572702
Tugrul Gungor <55763428+tugrulgungor@users.noreply.github.com>
2024-09-08 22:03:08 +0100

    Configuration.

---
+++ include/cronz/config.hpp
@@ -0,0 +1,21 @@
+/*
+ * Cronz - https://lib.cronz.dev
+ *
+ * Copyright (c) 2024 - present. All rights reserved.
+ * Tuğrul Güngör - https://tugrulgungor.me
+ *
+ * Distributed under the MIT License.
+ * https://opensource.org/license/mit/
+ */
+
+#ifndef CRONZ_CONFIG_HPP
+#define CRONZ_CONFIG_HPP 1
+
+/**
+ * @defgroup cronz_config Configuration
+ * @ingroup cronz
+ */
+
+#include "cronz/internal/config.hpp"
+
+#endif // CRONZ_CONFIG_HPP
