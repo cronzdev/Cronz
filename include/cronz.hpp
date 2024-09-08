f5c5eaa68f54bd16fc379daac67d9a1de028964c
Tugrul Gungor <55763428+tugrulgungor@users.noreply.github.com>
2024-09-08 22:01:40 +0100

    Create cronz.hpp

---
+++ include/cronz.hpp
@@ -0,0 +1,22 @@
+/*
+ * Cronz - https://lib.cronz.dev
+ *
+ * Copyright (c) 2024 - present. All rights reserved.
+ * Tuğrul Güngör - https://tugrulgungor.me
+ *
+ * Distributed under the MIT License.
+ * https://opensource.org/license/mit/
+ */
+
+#ifndef CRONZ_HPP
+#define CRONZ_HPP
+
+/**
+ * @defgroup cronz Cronz
+ */
+
+#include "cronz/config.hpp"
+#include "cronz/ip.hpp"
+#include "cronz/url.hpp"
+
+#endif // CRONZ_HPP
