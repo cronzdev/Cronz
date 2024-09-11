ab48ee5f4ad30071fb9c46bea624bee2b9c50bc7
Tugrul Gungor <55763428+tugrulgungor@users.noreply.github.com>
2024-09-11 02:22:45 +0100

    Create types.hpp

---
+++ include/cronz/crypto/types.hpp
@@ -0,0 +1,16 @@
+/*
+ * Cronz - https://lib.cronz.dev
+ *
+ * Copyright (c) 2024 - present. All rights reserved.
+ * Tuğrul Güngör - https://tugrulgungor.me
+ *
+ * Distributed under the MIT License.
+ * https://opensource.org/license/mit/
+ */
+
+#ifndef CRONZ_CRYPTO_TYPES_HPP
+#define CRONZ_CRYPTO_TYPES_HPP 1
+
+#include "cronz/internal/namespace.hpp"
+
+#endif // CRONZ_CRYPTO_TYPES_HPP
