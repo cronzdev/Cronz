a3e6ce9b5473615ebf641dd7914e71695b18ec66
Tugrul Gungor <55763428+tugrulgungor@users.noreply.github.com>
2024-09-11 02:22:36 +0100

    Create crypto.hpp

---
+++ include/cronz/crypto.hpp
@@ -0,0 +1,23 @@
+/*
+ * Cronz - https://lib.cronz.dev
+ *
+ * Copyright (c) 2024 - present. All rights reserved.
+ * Tuğrul Güngör - https://tugrulgungor.me
+ *
+ * Distributed under the MIT License.
+ * https://opensource.org/license/mit/
+ */
+
+#ifndef CRONZ_CRYPTO_HPP
+#define CRONZ_CRYPTO_HPP 1
+
+/**
+ * @defgroup cronz_crypto Cryptography
+ * @ingroup cronz
+ */
+
+#include "cronz/crypto/base64.hpp"
+#include "cronz/crypto/hex.hpp"
+#include "cronz/crypto/types.hpp"
+
+#endif // CRONZ_CRYPTO_HPP
