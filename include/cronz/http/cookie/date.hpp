912d153147ca679e952ed674a15ce05a23f5267f
Tugrul Gungor <55763428+tugrulgungor@users.noreply.github.com>
2025-04-09 12:13:53 +0100

    HTTP Cookie.

--- include/cronz/http/cookie/date.hpp
+++ include/cronz/http/cookie/date.hpp
@@ -14,7 +14,13 @@
 #include "cronz/http/date.hpp"
 
 CRONZ_BEGIN_HTTP_NAMESPACE
-    class CookieDate {};
+    /**
+     * @ingroup cronz_http
+     * @brief Cookie date type.
+     * @type DateTime<static_cast<DateConfigurationFlags>(0)> CookieDate
+     */
+    using CookieDate = DateTime<static_cast<DateTimeConfigurationFlags>(0)>;
+
 CRONZ_END_HTTP_NAMESPACE
 
 #endif // CRONZ_HTTP_COOKIE_DATE_HPP
