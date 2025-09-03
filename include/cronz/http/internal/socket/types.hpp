64a48dc1322f170ec3488632988f3504aa595dcb
Tugrul Gungor <55763428+tugrulgungor@users.noreply.github.com>
2025-09-03 20:41:21 +0100

    Minor fixes and refactoring.

--- include/cronz/http/internal/socket/types.hpp
+++ include/cronz/http/internal/socket/types.hpp
@@ -44,4 +44,40 @@ CRONZ_BEGIN_HTTP_INTERNAL_NAMESPACE
 CRONZ_END_HTTP_INTERNAL_NAMESPACE
 #endif // CRONZ_OS_WINDOWS && !CRONZ_OS_WINDOWS_CYGWIN
 
+#if CRONZ_OS_LINUX || CRONZ_OS_UNIX || CRONZ_OS_WINDOWS_CYGWIN
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <poll.h>
+#include <errno.h>
+
+CRONZ_BEGIN_HTTP_INTERNAL_NAMESPACE
+    using CRONZ_SOCKET = int;
+
+    inline static constexpr auto CRONZ_INVALID_SOCKET = -1;
+    inline static constexpr auto CRONZ_SOCKET_ERROR = -1;
+    inline static constexpr auto CRONZ_SOCKET_GET_ERROR = errno;
+    inline static constexpr auto CRONZ_CREATE_SOCKET = ::socket;
+    inline static constexpr auto CRONZ_CLOSE_SOCKET = ::close;
+    inline static constexpr auto CRONZ_BIND_SOCKET = ::bind;
+    inline static constexpr auto CRONZ_LISTEN_SOCKET = ::listen;
+    inline static constexpr auto CRONZ_READ_SOCKET = ::recv;
+    inline static constexpr auto CRONZ_WRITE_SOCKET = ::send;
+    inline static constexpr auto CRONZ_ACCEPT_SOCKET = ::accept;
+    inline static constexpr auto CRONZ_SHUTDOWN_SOCKET = ::shutdown;
+
+    inline static constexpr auto CRONZ_POLL = poll;
+    inline static constexpr auto CRONZ_POLL_M = 1;
+    inline static constexpr auto CRONZ_POLL_IN = POLLIN;
+    inline static constexpr auto CRONZ_POLL_OUT = POLLOUT;
+    inline static constexpr auto CRONZ_POLL_HUP = POLLHUP;
+    inline static constexpr auto CRONZ_POLL_ERR = POLLERR;
+
+    using CRONZ_POLL_STRUCT = struct pollfd;
+
+CRONZ_END_HTTP_INTERNAL_NAMESPACE
+
+#endif // CRONZ_OS_LINUX || CRONZ_OS_UNIX || CRONZ_OS_WINDOWS_CYGWIN
+
 #endif // CRONZ_HTTP_INTERNAL_SOCKET_TYPES_HPP
